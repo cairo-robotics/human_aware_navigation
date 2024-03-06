f836cdad3ac8800e524cc784efeddc99451e7df2
himanshugupta1009 <himanshu.gupta@colorado.edu>
2024-03-05 21:45:24 -0700

    Modify code to work with the modified BellmanPDEs.jl code. Use HJBPlanner

--- src/HJB_wrappers.jl
+++ src/HJB_wrappers.jl
@@ -1,21 +1,12 @@
-using BellmanPDEs
+import BellmanPDEs as BPDE
 using LazySets
-using GridInterpolations
-using StaticArraysCore
+using StaticArrays
 
-# function get_HJB_environment(vehicle_params,exp_details)
-#
-#     world = exp_details.env
-#     workspace = VPolygon([[0.0, 0.0], [world.length, 0.0], [world.length, world.breadth], [0.0, world.breadth]])
-#     obstacle_list = Array{Any,1}()
-#     for obs in world.obstacles
-#         push!(obstacle_list, VPolyCircle([obs.x,obs.y], obs.r+0.1))
-#     end
-#     goal = VPolyCircle([vehicle_params.goal.x, vehicle_params.goal.y], exp_details.radius_around_vehicle_goal)
-#     env = define_environment(workspace, obstacle_list, goal)
-#     return env
-# end
-
+struct HJBEnvironment{P,Q,R}
+    workspace::P
+    obstacle_list::Q
+    goal::R
+end
 
 function get_HJB_environment(vehicle_params,exp_details)
 
@@ -37,36 +28,42 @@ function get_HJB_environment(vehicle_params,exp_details)
     workspace_goal = (vehicle_params.goal.x, vehicle_params.goal.y)
     goal = VPolyCircle(workspace_goal, exp_details.radius_around_vehicle_goal)
 
-    env = define_environment(workspace, obstacle_list, goal)
+    env = HJBEnvironment(workspace, obstacle_list, goal)
     return env
 end
 
-# function get_HJB_vehicle(vehicle_params)
-#     body_dims = [vehicle_params.length, vehicle_params.breadth]
-#     origin_to_cent = [vehicle_params.dist_origin_to_center, 0.0]
-#     veh_body = define_vehicle(vehicle_params.wheelbase, body_dims, origin_to_cent, vehicle_params.max_steering_angle, vehicle_params.max_speed)
-#     return veh_body
-# end
-
-function get_HJB_vehicle(vehicle_params)
-    body_dims = SVector(vehicle_params.length, vehicle_params.breadth)
-    origin_to_cent = SVector(vehicle_params.dist_origin_to_center, 0.0) # (x,y) distance of the vehicle center to the origin of the vehicle which is the center point of the rear axis
-    veh_body = define_vehicle(vehicle_params.wheelbase, body_dims, origin_to_cent, vehicle_params.max_steering_angle, vehicle_params.max_speed)
-    return veh_body
+struct HJBVehicleParameters{T}
+    l::Float64
+    body_dims::SVector{2,Float64}
+    radius_vb::Float64
+    origin_to_cent::SVector{2,Float64}
+    origin_body::T
+    phi_max::Float64
+    delta_theta_max::Float64
+    v_max::Float64
+    delta_speed::Float64
 end
 
 
-# function get_state_grid(world, vehicle_params)
-#     state_space = [[0.0, world.length], [0.0, world.breadth], [-pi, pi], [0.0, vehicle_params.max_speed]]
-#     dx_sizes = [0.5, 0.5, deg2rad(18.0), 1/3]
-#     angle_wrap = [false, false, true, false]
-#     sg = define_state_grid(state_space, dx_sizes, angle_wrap)
-#     return sg
-# end
-
-function get_state_grid(state_space,dx_sizes,angle_wrap)
-    sg = define_state_grid(state_space, dx_sizes, angle_wrap)
-    return sg
+function get_HJB_vehicle(vehicle_params)
+    body_dims = SVector(vehicle_params.length, vehicle_params.breadth)
+    radius_around_vehicle = sqrt((0.5*body_dims[1])^2 + (0.5*body_dims[2])^2)
+    # (x,y) distance of the vehicle center to the origin of the vehicle which is the center point of the rear axis
+    origin_to_cent = SVector(vehicle_params.dist_origin_to_center, 0.0) 
+    vehicle_body_at_origin = get_vehicle_body(body_dims, origin_to_cent)
+    
+    veh_body = HJBVehicleParameters(
+                    vehicle_params.wheelbase,
+                    body_dims,
+                    radius_around_vehicle,
+                    origin_to_cent,
+                    vehicle_body_at_origin,
+                    vehicle_params.max_steering_angle,
+                    pi/4,
+                    vehicle_params.max_speed,
+                    0.5
+                    )
+    return veh_body
 end
 
 
@@ -95,7 +92,7 @@ function find_HJB_path(vehicle, vehicle_params, rollout_guide, exp_details)
     return path_x,path_y,path_theta
 end
 
-function rollout_get_actions(x, Dt, veh)
+function HJB_action_function(x, Dt, veh)
     # set change in velocity (Dv) limit
     Dv_lim = 0.5
     # set steering angle (phi) limit
@@ -142,12 +139,10 @@ function rollout_get_actions(x, Dt, veh)
         (phi_lim_p, Dv_lim)
         )
 
-    ia_set = SVector{num_actions,Int}(1:num_actions)
-
-    return actions,ia_set
+    return actions
 end
 
-function rollout_get_cost(x, a, Dt, veh)
+function HJB_cost_function(x, a, Dt, veh)
     # reward_x_a = 0
     # low speed penalty
     # reward_x_a += -(veh.v_max - abs(x[4] + a[2]))/veh.v_max
@@ -213,57 +208,37 @@ rollout_policy(START_X, -0.5, rollout_get_actions, rollout_get_cost, 0.5, rollou
 =#
 
 
-function even_better_reactive_policy(x_k::SVector{4,Float64}, Dv_RC::Float64, safe_value_lim::Float64,
-                        Dt::Float64, q_value_array::Array{Array{Float64, 1}, 1},
-                        value_array::Array{Float64,1}, veh::VehicleBody, sg::StateGrid)
-#sg::StateGrid{P,Q}) where {P,Q}
-    # get actions for current state
-    # c = 11
-    # for i in 1:10
-    #     c+=i
-    # end
+function get_HJB_rollout_guide(HJB_planning_details, exp_details, vehicle_params)
 
-    # c = mirror( (x_k,Dv_RC,safe_value_lim,Dt,q_value_array,value_array,veh,sg) )
-    # return c
-    actions, ia_set = rollout_get_actions(x_k, Dt, veh)
+    Δt = HJB_planning_details.Dt
+    ϵ = HJB_planning_details.Dval_tol
+    max_solve_steps = HJB_planning_details.max_solve_steps
+    world = exp_details.env
 
-    velocity_set = Tuple(Dv_RC)
-    val_x_RC, ia_RC = new_optimize_action(x_k, velocity_set, ia_set, actions, Dt, value_array, veh, sg)
+    l = world.length
+    b = world.breadth
+    max_speed = vehicle_params.max_speed
+    state_range = SVector{4,Tuple{Float64,Float64}}([
+                    (0.0,l), #Range in x
+                    (0.0,b), #Range in y
+                    (-pi,pi), #Range in theta
+                    (0.0,max_speed) #Range in v
+                    ])
+    dx_sizes = SVector(0.5, 0.5, deg2rad(18.0), 0.5)
 
-    # check if [Dv_RC, phi_best_RC] is a valid action in static environment ---
-    if val_x_RC >= safe_value_lim
-        a_ro = actions[ia_RC]
-        return a_ro
-    end
+    HJB_env = get_HJB_environment(vehicle_params,exp_details)
+    HJB_veh = get_HJB_vehicle(vehicle_params)
 
-    # B) if RC action is not valid, then find pure HJB best action ---
-    velocity_set = (0.0,-Dv_RC)
-    val_x_RC, ia_HJB = new_optimize_action(x_k, velocity_set, ia_set, actions, Dt, value_array, veh, sg)
-    a_ro = actions[ia_HJB]
+    P = BPDE.Problem(state_range,dx_sizes,HJB_env,HJB_veh,HJB_action_function,HJB_cost_function)
 
-    return a_ro
-end
+    hjb_solver = BPDE.HJBSolver(P,
+                max_steps=max_solve_steps,
+                ϵ=ϵ,
+                Δt=Δt
+                )
+    BPDE.solve(hjb_solver,P)
 
-function new_optimize_action(x::SVector{4,Float64}, Dv_RC::NTuple{K,Float64}, ia_set::SVector{L,Int},
-                    actions::SVector{M,N},Dt::Float64,
-                    value_array::Array{Float64,1}, veh::VehicleBody, sg::StateGrid) where {K,L,M,N}
-#sg::StateGrid{P,Q}) where {K,L,M,N,P,Q}
-
-    best_val = -Inf
-    best_action_index = -1
-    # iterate through all given action indices
-    for ja in eachindex(ia_set)
-        a = actions[ia_set[ja]]
-        if(a[2] in Dv_RC)
-            reward_x_a = rollout_get_cost(x, a, Dt, veh)
-            x_p, _ = propagate_state(x, a, Dt, veh)
-            val_xp = reward_x_a + interp_value(x_p, value_array, sg)
-            if(val_xp > best_val)
-                best_val = val_xp
-                best_action_index = ja
-            end
-        end
-    end
-
-    return best_val, best_action_index
+    safe_value_limit = 750.0
+    hjb_planner = BPDE.HJBPlanner(hjb_solver,P,safe_value_limit)
+    return hjb_planner
 end
